c8506f7699a2c44110b3fb642fdf299d30110980
wuxiaomeng94 <806620602@qq.com>
2019-09-29 13:02:34 +0800

    update

--- src/main/java/opensource/capinfo/uploader/uploadHandle/FtpUploaderHandle.java
+++ src/main/java/opensource/capinfo/uploader/uploadHandle/FtpUploaderHandle.java
@@ -1,6 +1,8 @@
 package opensource.capinfo.uploader.uploadHandle;
 
 import opensource.capinfo.utils.FTPUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
